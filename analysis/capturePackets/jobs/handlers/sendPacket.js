d5473b6dcd4d4cbacb7b22a93e13dadfc49981e3
dkart76ai <dkarlitos76@gmail.com>
2026-04-19 20:26:42 -0500

    test sending packet 311 before 312

--- analysis/capturePackets/jobs/handlers/sendPacket.js
+++ analysis/capturePackets/jobs/handlers/sendPacket.js
@@ -16,18 +16,20 @@ const HEADERS = {
 
 let captureIndex = 0
 let saveFileIndex = 0
+let captures = []
 
 const SAVE_DIR = path.join(__dirname, '../../packetSender')
 
-function savePacketToFile(url, tiles, request, response) {
-  let captures = []
+function savePacketToFile(opcode, url, tiles, request, response) {
   captures.push({
     id: ++captureIndex,
-    opcode: 312,
+    opcode,
     url,
-    tiles,
+    status: 200,
+    tileIds: tiles,
     request: {
-      bodyBufferB64: Buffer.from(request).toString('base64')
+      method: 'POST',
+      bodyB64: Buffer.from(request).toString('base64')
     },
     response: {
       bodyB64: Buffer.from(response).toString('base64')
@@ -38,21 +40,40 @@ function savePacketToFile(url, tiles, request, response) {
     fs.mkdirSync(SAVE_DIR, { recursive: true })
   }
 
-  const saveFile = path.join(SAVE_DIR, `pktsend-${String(saveFileIndex++).padStart(3, '0')}.json`)
+  const saveFile = path.join(SAVE_DIR, `pktsend-${String(saveFileIndex).padStart(3, '0')}.json`)
   //save packet to analyze
   fs.writeFileSync(
     saveFile,
     JSON.stringify(captures, (k, v) => (typeof v === 'bigint' ? v.toString() : v), 2)
   )
+
+  if (captures.length >= 30) {
+    captures = []
+    captureIndex = 0
+    saveFileIndex++
+    console.log(
+      `[${new Date().toLocaleTimeString()}] Rotation: switched to pktsend-${String(saveFileIndex).padStart(3, '0')}.json`
+    )
+  }
 }
 
-function buildPacketPayload(tiles, tokenBigInt, token2) {
+function buildPacket311Payload(tiles, tokenBigInt, token2) {
   if (!tokenBigInt || !token2) return null
 
   const randomSeq = Math.floor(Math.random() * 32000) + 1
+  const packetData = [[311, randomSeq, [[tokenBigInt], token2], ''], [[tiles]]]
+
+  return packetData
+}
+
+function buildPacket312Payload(tiles, tokenBigInt, token2) {
+  if (!tokenBigInt || !token2) return null
+
+  const zeros = new Array(tiles.length).fill(0)
+  const randomSeq = Math.floor(Math.random() * 32000) + 1
   const packetData = [
     [312, randomSeq, [[tokenBigInt], token2], ''],
-    [tiles, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [], []]
+    [tiles, zeros, [], []]
   ]
 
   return packetData
@@ -63,7 +84,7 @@ async function sendPacketHandler(payload) {
 
   const redisClient = getRedis()
 
-  const _token1 = await redisClient.get('mysession:token1:BigInt')
+  const _token1 = await redisClient.get('myPlayerId:BigInt')
   if (!_token1) {
     throw new Error('no session token1')
   }
@@ -76,35 +97,63 @@ async function sendPacketHandler(payload) {
   try {
     const token1 = BigInt(_token1)
     const token2 = new Uint8Array(_token2)
+    const url = kingdomUrls[kingdom]
+    console.log(`[SendPacket] Sending packets to ${url}  `)
 
-    const packetData = buildPacketPayload(tiles, token1, token2)
+    //---------- send packet 311
+    const packetData311 = buildPacket311Payload(tiles, token1, token2)
 
     // Encode the packet
-    const encoded = encodeMsgPack2MultiFragments(packetData)
+    const encoded311 = encodeMsgPack2MultiFragments(packetData311)
 
     // Send to server
-    const url = kingdomUrls[kingdom]
-    console.log(`[SendPacket] Sending packet to ${url}  `)
-    const response = await fetch(url, {
+    console.log(`[SendPacket] Sending packet311 to ${url}  `)
+    const response311 = await fetch(url, {
       method: 'POST',
       headers: HEADERS,
-      body: encoded
+      body: encoded311
     })
 
-    if (!response.ok) {
-      throw new Error(`Server returned ${response.status}: ${response.statusText}`)
+    if (!response311.ok) {
+      throw new Error(`Server returned ${response311.status}: ${response311.statusText}`)
     }
 
     // Get response buffer
-    const buffer = await response.arrayBuffer()
-    const bytes = new Uint8Array(buffer)
+    const buffer311 = await response311.arrayBuffer()
+    const bytes311 = new Uint8Array(buffer311)
 
-    savePacketToFile(url, tiles, encoded, bytes)
+    savePacketToFile(311, url, tiles, encoded311, bytes311)
+
+    //------ send packet 312
+    const packetData312 = buildPacket312Payload(tiles, token1, token2)
+
+    // Encode the packet
+    const encoded312 = encodeMsgPack2MultiFragments(packetData312)
+
+    // Send to server
+    console.log(`[SendPacket] Sending packet312 to ${url}  `)
+    const response312 = await fetch(url, {
+      method: 'POST',
+      headers: HEADERS,
+      body: encoded312
+    })
+
+    if (!response312.ok) {
+      throw new Error(`Server returned ${response312.status}: ${response312.statusText}`)
+    }
+
+    // Get response buffer
+    const buffer312 = await response312.arrayBuffer()
+    const bytes312 = new Uint8Array(buffer312)
+
+    savePacketToFile(312, url, tiles, encoded312, bytes312)
+
+    //-----
 
     const msgPackDataKey = `msgPackData:${Date.now()}:${Math.random().toString(36).substring(7)}`
 
     // Store raw bytes (Uint8Array), not decoded results
-    await redisClient.set(msgPackDataKey, bytes, 'EX', 60 * 10)
+    await redisClient.set(msgPackDataKey, bytes312, 'EX', 60 * 10)
 
     const result = {
       success: true,
